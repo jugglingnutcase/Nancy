762ff079eacb3cb79955f09354e61e19c65f063b
James R Sconfitto <james@exele.com>
2013-11-25 08:52:20 -0500

    Add tests for new root namespace behavior

--- src/Nancy.Embedded.Tests/Unit/EmbeddedViewLocationProviderFixture.cs
+++ src/Nancy.Embedded.Tests/Unit/EmbeddedViewLocationProviderFixture.cs
@@ -169,5 +169,78 @@ namespace Nancy.Tests.Unit.ViewEngines
             A.CallTo(() => this.reader.GetResourceStreamMatches(this.GetType().Assembly, A<IEnumerable<string>>._)).MustNotHaveHappened();
             A.CallTo(() => this.reader.GetResourceStreamMatches(typeof(NancyEngine).Assembly, A<IEnumerable<string>>._)).MustHaveHappened();
         }
+
+        [Fact]
+        public void Should_not_throw_invalid_operation_exception_if_only_one_view_was_found_and_no_root_namespace_has_been_defined()
+        {
+            // Given
+            var extensions = new[] { "html" };
+
+            EmbeddedViewLocationProvider.RootNamespaces.Remove(this.GetType().Assembly);
+
+            var match = new Tuple<string, Func<StreamReader>>(
+                "Some.Resource.View.html",
+                () => null);
+
+            A.CallTo(() => this.reader.GetResourceStreamMatches(A<Assembly>._, A<IEnumerable<string>>._)).Returns(new[] { match });
+
+            // When
+            var exception = Record.Exception(() => this.viewProvider.GetLocatedViews(extensions).ToList());
+
+            // Then
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void Should_retrieve_single_view_with_no_root_namespaces()
+        {
+            // Given
+            var extensions = new[] { "html" };
+
+            EmbeddedViewLocationProvider.RootNamespaces.Remove(this.GetType().Assembly);
+
+            var match = new Tuple<string, Func<StreamReader>>(
+                "Some.Resource.View.html",
+                () => null);
+
+            A.CallTo(() => this.reader.GetResourceStreamMatches(A<Assembly>._, A<IEnumerable<string>>._)).Returns(new[] { match });
+
+            // When
+            var result = this.viewProvider.GetLocatedViews(extensions);
+
+            // Then
+            result.First().Name.ShouldEqual("View");
+        }
+
+        [Fact]
+        public void Should_retrieve_views_with_no_root_namespaces()
+        {
+            // Given 2 views with no available root namespaces
+            var extensions = new[] { "html" };
+
+            EmbeddedViewLocationProvider.RootNamespaces.Remove(this.GetType().Assembly);
+
+            var viewMatch1 = new Tuple<string, Func<StreamReader>>(
+                "Some.Resource.View.html",
+                () => null);
+
+            var viewMatch2 = new Tuple<string, Func<StreamReader>>(
+                "Some.Resource.Path.With.Sub.Folder.View.html",
+                () => null);
+
+            A.CallTo(() => this.reader.GetResourceStreamMatches(A<Assembly>._, A<IEnumerable<string>>._)).Returns(new[] { viewMatch1, viewMatch2 });
+
+            // When
+            var result = this.viewProvider.GetLocatedViews(extensions);
+
+            // Then
+            result.Count().ShouldEqual(2);
+
+            // Both views should be named "View"
+            result.Where(view => view.Name == "View").Count().ShouldEqual(2);
+
+            // One of the views should have the folder name as its location
+            result.Where(view => view.Location == "Path/With/Sub/Folder").Count().ShouldEqual(1);
+        }
     }
 }
