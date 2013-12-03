0f7b4fe9595ea955846cac7b47f9d9d0568adf21
Kristian Hellang <kristian@hellang.com>
2013-12-03 19:05:03 +0100

    Added demo project for embedded resources and views

---
+++ src/Nancy.Demo.Embedded/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Demo.Embedded
+{
+    using System;
+
+    using Nancy.Hosting.Self;
+
+    public static class Program
+    {
+        private const string BaseUri = "http://localhost:8080/";
+
+        public static void Main()
+        {
+            using (var host = new NancyHost(new Uri(BaseUri)))
+            {
+                host.Start();
+                Console.WriteLine("Nancy listening on " + BaseUri);
+                Console.ReadLine();
+            }
+        }
+    }
+}
